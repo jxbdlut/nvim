3d50268f8308e0034852d204f17b2f45ba697a2c
jxbdlut <jxbdlut@qq.com>
2023-05-01 23:14:05 +0800

    auto session log 改成error

--- lua/plugin-config/auto-session.lua
+++ lua/plugin-config/auto-session.lua
@@ -5,7 +5,7 @@ if not status then
 end
 
 auto_session.setup({
-	log_level = "debug",
+	log_level = "error",
 	-- 打开这些目录里面的内容时，不加载会话
 	auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/", "/ect", "/usr" },
 	auto_session_enable_last_session = false,
